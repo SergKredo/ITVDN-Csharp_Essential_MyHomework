bd4180caa0073715e3a6e3fd2948e7be8800a10c
SergioKredentser <40566180+SergKredo@users.noreply.github.com>
2020-02-27 17:52:18 +0200

    StopWatch in WPF

---
+++ Lesson12_Homework/StopWatch(Task3)/Model.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace StopWatch_Task3_
+{
+    class Model
+    {
+        public string AddNumber(string s)   // Метод, который инкрементирует значение счета таймера на одну единицу
+        {
+            int number = Convert.ToInt32(s);
+            
+                ++number;
+                s = Convert.ToString(number);
+                if (s.Length == 1)
+                {
+                    s = "0" + s;
+                }
+            return s;
+        }
+    }
+}
