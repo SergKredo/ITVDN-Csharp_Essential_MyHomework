8fd126c8bfe95357c2df3f980abe6834025166bd
SergioKredentser <40566180+SergKredo@users.noreply.github.com>
2020-02-27 23:25:06 +0200

    Add files via upload

--- Lesson12_Homework/StopWatch(Task3)/obj/Debug/StopWatch(Task3)_Content.g.i.cs
+++ Lesson12_Homework/StopWatch(Task3)/obj/Debug/StopWatch(Task3)_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("pngfuel.com.ico")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("stopwatch.ico")]
 
 
