f10d33afaa80d1c0389ce8873ed9d3979e5dffc8
SergioKredentser <40566180+SergKredo@users.noreply.github.com>
2020-04-03 15:52:26 +0300

    Add files via upload

---
+++ Lesson18_Homework/Additional Task/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyNamespace;     // Импорт в проект собственного пространства имен
+
+
+
+namespace MyNamespace  // Создание собственного пространства имен
+{
+    static class MyClass   // Статический класс
+    {
+        public static string ExtensionMethod(this string param)   // Расширяющий метод для конкатенации входящего аргумента с заданным выражением литералов
+        {
+            return "*****" + param + "*****";
+        }
+    }
+}
+
+
+namespace Additional_Task
+{
+    /*Создайте собственное пространство имен MyNamespace с классом MyClass и подключите его в другом
+приложении.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)  // Бесконечный цикл
+            {
+                Console.WriteLine(new string('-', 30));
+                Console.Write(string.Format("Enter the word: ".ToUpper()));
+                string word = Console.ReadLine();
+                Console.WriteLine(string.Format("Result after extension method: {0}", word.ExtensionMethod()));  // Использование расширяющего метода для введенного пользователем выражения
+                Console.WriteLine(new string('-', 30));
+            }
+        }
+    }
+}
